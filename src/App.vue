2e975d60373c361f2d94308635d35002cb5816cb
Jorge Reyes <jorge_irving_15@hotmail.com>
2022-01-15 15:27:30 -0300

    ADD: componets task parent

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,16 @@
 <template>
   <div id="app">
-    <section>
-      <label for="txt_task">Tarea</label>
-      <input
-        id="txt_task"
-        type="text"
-        v-model="task"
-        placeholder="ingrese nueva tarea"
-      />
-      <button @click="addTask">Agregar</button>
-    </section>
-    <section>
-      <TodoList :tasks="tasks" @deleteTaks="deleteTaks" />
-    </section>
+    <Task />
   </div>
 </template>
 
 <script>
-import TodoList from "@/components/TodoList.vue";
+import Task from "@/components/Task.vue";
 
 export default {
   name: "App",
   components: {
-    TodoList,
-  },
-  data() {
-    return {
-      task: "",
-      tasks: [],
-    };
-  },
-  methods: {
-    addTask() {
-      const { task } = this;
-      this.tasks.push(task);
-    },
-    deleteTaks(indexTask) {
-      this.tasks.splice(indexTask, 1);
-    },
+    Task,
   },
 };
 </script>
@@ -50,21 +23,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-label {
-  font-weight: 700;
-  margin: 0rem 1rem;
-}
-
-button {
-  padding: 0.1rem 0.75rem;
-  margin: 0rem 1rem;
-}
-
-section {
-  display: flex;
-  align-items: center;
-  justify-content: center;
 }
 </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <section>
+      <label for="txt_task">Tarea</label>
+      <input id="txt_task" type="text" v-model="task" placeholder="ingrese nueva tarea" />
+      <button @click="addTask">Agregar</button>
+    </section>
+    <section v-if="tasks.length > 0">
+      <TodoList :tasks="tasks" @deleteTaks="deleteTaks" />
+    </section>
+  </div>
+</template>
+
+<script>
+import TodoList from "@/components/TodoList.vue";
+
+export default {
+  name: "Task",
+  components: {
+    TodoList,
+  },
+  data() {
+    return {
+      task: "",
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask() {
+      const { task } = this;
+      this.tasks.push(task);
+    },
+    deleteTaks(indexTask) {
+      this.tasks.splice(indexTask, 1);
+    },
+  },
+};
+</script>
+
+<style scoped>
+label {
+  font-weight: 700;
+  margin: 0rem 1rem;
+}
+
+button {
+  padding: 0.1rem 0.75rem;
+  margin: 0rem 1rem;
+}
+
+section {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
